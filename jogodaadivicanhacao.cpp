b0d573b0798402e66a9fc86b2b0ea72c241c6d28
Thiago da Silva Geisler <contato.thiagogeisler@gmail.com>
2022-05-13 20:55:12 -0300

    Projeto criado

---
+++ jogodaadivicanhacao.cpp
@@ -0,0 +1,7 @@
+
+#include<iostream>
+int main() {
+std::cout << "**************************************" << std::endl;  
+std::cout << "* Bem-vindos ao jogo da adivinhação! *" << std::endl;
+std::cout << "**************************************" << std::endl;
+}
